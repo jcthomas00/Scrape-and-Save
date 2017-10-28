c519286675e005f6090823cfcb0a76f7954fa09f
Jacob Thomas <jcthomas00@gmail.com>
2017-10-27 21:27:47 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require(`express`),
+  express-handlebars = require(`express-handlebars`),
+  mongoose = require(`mongoose`),
+  body-parser = require(`body-parser`),
+  cheerio = require(`cheerio`),
+  request = require(`request`);
+
